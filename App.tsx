7f050ae6967bcb94037c9ae7a267c019dd9175a0
Wallace Rossini <wallace.costa@compasso.com.br>
2022-07-18 16:44:58 -0300

    Aula 2 - Início de projeto

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { NativeBaseProvider, StatusBar } from 'native-base'
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { SignIn } from './src/screens/SignIn';
+import { THEME } from './src/styles/theme';
+import { Loading } from './src/components/Loading';
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({Roboto_400Regular,Roboto_700Bold})
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar barStyle='light-content' backgroundColor="transparent"/>
+     {fontsLoaded ? <SignIn /> : <Loading/> }
+    </NativeBaseProvider>
+  );
+}
+
